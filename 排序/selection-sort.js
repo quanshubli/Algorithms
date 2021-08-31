5be88155d2186e7d2aa73341a473152b2a7ed415
quanshubli <309063267@qq.com>
2021-08-31 23:49:27 +0800

    add:选择排序

---
+++ 排序/selection-sort.js
@@ -0,0 +1,30 @@
+/**
+ * 选择排序
+ * @param {array}} list 
+ */
+function selectionSort(list) {
+  const result = []
+  const copyList = [...list]
+  let index
+  let length = copyList.length
+
+  while (length > 0) {
+    index = 0
+
+    for (let i = 0; i < length; i++) {
+      if (copyList[i] < copyList[index]) {
+        index = i
+      }
+    }
+
+    result.push(copyList.splice(index, 1)[0])
+    length--
+  }
+
+  return result
+}
+
+// 用例
+// const list = ['S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E']
+// const result = selectionSort(list)
+// console.log(result)
